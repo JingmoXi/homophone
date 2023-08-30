354f04921658f1cf357504bcb50649f2c8d801b3
wangzijie <1404081737@qq.com>
2023-08-30 17:41:31 +0800

    init

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,14 +1,10 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::ffi::{c_char, CString, CStr};
+
+#[no_mangle]
+pub extern "C" fn md5(data: *const c_char) -> *mut c_char {
+    let cstr = unsafe { CStr::from_ptr(data)};
+    let rstr = String::from_utf8_lossy(cstr.to_bytes()).to_string();
+    let digest = md5::compute(rstr);
+    CString::new(format!("{:x}", digest)).unwrap().into_raw()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
